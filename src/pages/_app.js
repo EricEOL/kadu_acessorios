97a562aeb44f109769740980a7d35f600f38b05d
EricEOL <lopes.eric051@gmail.com>
2021-05-22 17:19:20 -0300

    Add og

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,25 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Kadu Acessórios - Mecânica de automóveis</title>
+
+        <meta property="og:image" content="/images/car-bg2" key="ogimage" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://kadu-acessorios.vercel.app/" />
+        <meta property="og:title" content="Kadu Acessórios - Mecânica de automóveis" key="ogtitle" />
+        <meta property="og:description" content="A oficina mecânica que vai dar um jeito no seu carro!" key="ogdesc" />
+
+      </Head>
+
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   )
 }
 
