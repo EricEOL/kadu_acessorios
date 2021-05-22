804c9e86ac3a54613ebf90309fcdb49885257292
EricEOL <lopes.eric051@gmail.com>
2021-05-22 17:21:34 -0300

    Ajustando og-image

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Kadu Acessórios - Mecânica de automóveis</title>
 
-        <meta property="og:image" content="/images/car-bg2" key="ogimage" />
+        <meta property="og:image" content="https://images.pexels.com/photos/638479/pexels-photo-638479.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" key="ogimage" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://kadu-acessorios.vercel.app/" />
         <meta property="og:title" content="Kadu Acessórios - Mecânica de automóveis" key="ogtitle" />
